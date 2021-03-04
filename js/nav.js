c91c61bb63c2fb63a5874fb34f581097517f3cc0
Zak <rememberds1@gmail.com>
2021-03-04 23:35:39 +0800

    Nav bar :)

---
+++ js/nav.js
@@ -0,0 +1,35 @@
+// Create Element Variables
+const navbar = document.querySelector(".top-nav")
+const navExpander = document.createElement("li")
+const navExpanderLink = document.createElement("a")
+
+// Set Element Attributes
+navExpander.setAttribute("class", "nav-mobile-expander")
+navExpanderLink.setAttribute("href", "javascript:void(0)")
+
+// Mousedown Event Listener
+navExpanderLink.addEventListener("mousedown", () => {
+    // Loop through all nav-items, except for the first one
+    for (var i = 1; i < navbar.querySelectorAll(".nav-item").length; i++)
+        // Toggles the class 'nav-mobile-hidden'
+        navbar.querySelectorAll(".nav-item")[i].classList.toggle("nav-mobile-hidden")
+    
+    // Toggle the text
+    if (navExpanderLink.innerText == "V")
+        navExpanderLink.innerText = "^"
+    else
+        navExpanderLink.innerText = "V"
+})
+
+// Add nav expander link
+navExpander.append(navExpanderLink)
+navExpanderLink.innerText = "V"
+
+window.onload = () => {
+    // Loop through all nav-items, except for the first one
+    for (var i = 1; i < navbar.querySelectorAll(".nav-item").length; i++)
+        // Toggles the class 'nav-mobile-hidden'
+        navbar.querySelectorAll(".nav-item")[i].classList.toggle("nav-mobile-hidden")
+    // Add navExpander element to the nav bar
+    navbar.append(navExpander)
+}
