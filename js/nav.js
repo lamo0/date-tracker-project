626df916a254c027ebb2c178ec048c35f5814bea
Zak <rememberds1@gmail.com>
2021-03-05 20:08:19 +0800

    Updated Nav Bar

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,13 @@
 // Create Element Variables
 const navbar = document.querySelector(".top-nav")
 const navExpander = document.createElement("li")
-const navExpanderLink = document.createElement("a")
+const navExpanderLink = document.createElement("span")
 
 // Set Element Attributes
 navExpander.setAttribute("class", "nav-mobile-expander")
-navExpanderLink.setAttribute("href", "javascript:void(0)")
 
 // Mousedown Event Listener
-navExpanderLink.addEventListener("mousedown", () => {
+navExpander.addEventListener("mousedown", () => {
     // Loop through all nav-items, except for the first one
     for (var i = 1; i < navbar.querySelectorAll(".nav-item").length; i++)
         // Toggles the class 'nav-mobile-hidden'
@@ -28,5 +27,5 @@ window.onload = () => {
         // Toggles the class 'nav-mobile-hidden'
         navbar.querySelectorAll(".nav-item")[i].classList.toggle("nav-mobile-hidden")
     // Add navExpander element to the nav bar
-    navbar.append(navExpander)
+    navbar.querySelectorAll(".nav-item")[0].append(navExpander)
 }
